2803812b7ebf595a5f194d65c2636ceccb0d0a89
Pratik Singh <pratiktsingh17@gmail.com>
2025-10-25 13:47:18 +0300

    Added write_file function

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,1 +1,18 @@
-def write_file(working_directory, file_path, content)
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(abs_file_path, 'w') as file:
+            file.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Exception writing to file {e}'
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
     working_directory = "calculator"
@@ -12,11 +13,19 @@ def main():
     # pkg_contents = get_files_info(working_directory, "../")
     # print(pkg_contents)
 
-    print(get_file_content(working_directory, "lorem.txt"))
-    print(get_file_content(working_directory, "main.py"))
-    print(get_file_content(working_directory, "pkg/calculator.py"))
-    print(get_file_content(working_directory, "pkg/nonon.py"))
-    print(get_file_content(working_directory, "/bin/cat"))
+    # print(get_file_content(working_directory, "lorem.txt"))
+    # print(get_file_content(working_directory, "main.py"))
+    # print(get_file_content(working_directory, "pkg/calculator.py"))
+    # print(get_file_content(working_directory, "pkg/nonon.py"))
+    # print(get_file_content(working_directory, "/bin/cat"))
 
+    # write_file(working_directory, "demo.txt", "Hello, World!")
+
+    # print(get_file_content(working_directory, "demo.txt"))
+
+    print(write_file("calculator", "demo.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print(write_file("calculator", "/tmp", "this should not be allowed"))
 
 main()
